9368883854f1acf321f955bb217634d7fda0e1db
Jacob Quant <jacobq@gmail.com>
2020-05-14 15:35:11 -0500

    won't fallback when locale is undefined

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,6 +5,18 @@ export default class ApplicationRoute extends Route {
   @service intl;
   beforeModel() {
     super.beforeModel(...arguments);
-    this.intl.setLocale(['fr-fr']);
+    // No locale defined. Unable to resolve translation: "hello.world"
+    //this.intl.setLocale();
+    //this.intl.setLocale(null);
+    this.intl.setLocale([]);
+
+    // Missing translation "hello.world" for locale ""
+    //this.intl.setLocale(['']);
+
+    // Missing translation "hello.world" for locale "zh-cn" 
+    //this.intl.setLocale(['zh-CN']);
+
+    // Works! ("Hello World!")
+    //this.intl.setLocale(['fr-fr']);
   }
 }
